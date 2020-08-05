16476256ecbce3be5cd46e35b1da124496076ed9
Aykut <aykut0171@gmail.com>
2020-08-05 21:09:33 +1000

    vars

---
+++ vars/Program.cs
@@ -0,0 +1,32 @@
+﻿// DONT Start With
+// Number
+// Space
+// Specific language
+
+using System;
+
+namespace vars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //integer
+            int number1 = 10;
+            number1 = 20;
+            long num2 = 100;
+            short num3 = 5;
+            byte num4 = 10;
+            //float
+            float a = 10.5f;
+            double b = 20.6;
+            decimal c = 100.56m;
+
+            Console.WriteLine(number1);
+            System.Console.WriteLine(a);
+            System.Console.WriteLine(b);
+            System.Console.WriteLine(c);
+
+        }
+    }
+}
